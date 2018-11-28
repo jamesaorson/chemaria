f21ea92aa638b56538e9c3be16ee3ac9c619c443
James Osborne <james.osborne112@gmail.com>
2018-11-28 12:16:38 -0500

    Make chunks index by position. Player can destroy block. Toolbar shows text.

--- modules/models/Chunk.lua
+++ modules/models/Chunk.lua
@@ -16,30 +16,26 @@ function Chunk:add_block(blockToAdd)
 	if not self.blocks then
 		self.blocks = {}
 	end
-	table.insert(self.blocks, blockToAdd)
+	if not self.blocks[blockToAdd.position.x] then
+		self.blocks[blockToAdd.position.x] = {}
+	end
+	self.blocks[blockToAdd.position.x][blockToAdd.position.y] = blockToAdd
 end
 
 function Chunk:remove_block(blockToRemove)
-	removed  = false
-	for i, block in ipairs(self.blocks) do
-		if block == blockToRemove then
-			table.remove(self.blocks, i)
-			removed = true
-			break
-		end
-	end
-
-	return removed
+	return Chunk:remove_block_at_position(blockToRemove.position)
 end
 
 function Chunk:remove_block_at_position(position)
-	removed = false
-	for i, block in ipairs(self.blocks) do
-		if block.position == position then
-			table.remove(self.blocks, i)
-			removed = true
-			break
+	removed  = false
+	pprint(position)
+	if self.blocks[position.x][position.y] then
+		table.remove(self.blocks[position.x], position.y)
+		print(table.getn(self.blocks[position.x]))
+		if table.getn(self.blocks[position.x]) == 0 then
+			table.remove(self.blocks, position.x)
 		end
+		removed = true
 	end
 	return removed
 end
