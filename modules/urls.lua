c3bf6cd7e7df519b73b36fd94c99ce51a41bac01
JamesOsborne <james.osborne112@gmail.com>
2018-12-02 10:55:21 -0500

    Change inventory slot to be a template

--- modules/urls.lua
+++ modules/urls.lua
@@ -1,7 +1,21 @@
+local constants = require "modules.constants"
+
 local M = {}
+
+local function calculate_inventory_slot_index(i, j)
+	return (i - 1) * constants.INVENTORY_SIZE.rowSize + j
+end
 
 function M.get_factory_url(objectName)
 	return "/"..objectName.."_factory#"..objectName.."_factory"
+end
+
+function M.get_inventory_node_url(i, j)
+	return "inventory/inventory_slot_" .. calculate_inventory_slot_index(i, j) .. "/slot"
+end
+
+function M.get_inventory_text_node_url(i, j)
+	return "inventory/inventory_slot_" .. calculate_inventory_slot_index(i, j) .. "/text"
 end
 
 function M.get_toolbar_node_url(index)
