0beea9ddf5b21d404e44e8b269fd4bfd5ecd08d5
JamesOsborne <james.osborne112@gmail.com>
2018-12-02 10:40:25 -0500

    Change toolbar slot to be a template

--- modules/urls.lua
+++ modules/urls.lua
@@ -4,4 +4,12 @@ function M.get_factory_url(objectName)
 	return "/"..objectName.."_factory#"..objectName.."_factory"
 end
 
+function M.get_toolbar_node_url(index)
+	return "toolbar/toolbar_slot_" ..  index .. "/slot"
+end
+
+function M.get_toolbar_text_node_url(index)
+	return "toolbar/toolbar_slot_" ..  index .. "/text"
+end
+
 return M
