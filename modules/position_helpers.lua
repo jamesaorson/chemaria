e2ab8477fb73f1b7391c042e978889db77c73d7e
JamesOsborne <james.osborne112@gmail.com>
2019-02-26 21:47:13 -0500

    Rename world_generation to world

--- modules/position_helpers.lua
+++ modules/position_helpers.lua
@@ -1,7 +1,7 @@
 local camera_constants = require "modules.constants.camera"
 local helpers = require "modules.helpers"
 local orthographic = require "orthographic.camera"
-local world_generation_constants = require "modules.constants.world_generation"
+local world_constants = require "modules.constants.world"
 
 local M = {}
 
@@ -10,7 +10,7 @@ local M = {}
 --------------------------
 
 function M.block_to_screen_position(block)
-	return vmath.vector3(block.position.x + (block.chunk.x) * world_generation_constants.CHUNK_SIZE, block.position.y + (block.chunk.y) * world_generation_constants.CHUNK_SIZE, 0) * world_generation_constants.BLOCK_SIZE
+	return vmath.vector3(block.position.x + (block.chunk.x) * world_constants.CHUNK_SIZE, block.position.y + (block.chunk.y) * world_constants.CHUNK_SIZE, 0) * world_constants.BLOCK_SIZE
 end
 
 ------------------------------
@@ -40,8 +40,8 @@ end
 function M.screen_to_block_position(position)
 	position = orthographic.screen_to_world(camera_constants.CAMERA_ID, position)
 	local blockPosition =  vmath.vector3(
-		helpers.round(position.x / world_generation_constants.BLOCK_SIZE) % world_generation_constants.CHUNK_SIZE,
-		helpers.round(position.y / world_generation_constants.BLOCK_SIZE) % world_generation_constants.CHUNK_SIZE,
+		helpers.round(position.x / world_constants.BLOCK_SIZE) % world_constants.CHUNK_SIZE,
+		helpers.round(position.y / world_constants.BLOCK_SIZE) % world_constants.CHUNK_SIZE,
 		0)
 	return blockPosition
 end
@@ -50,8 +50,8 @@ function M.screen_to_chunk_position(position)
 	position = orthographic.screen_to_world(camera_constants.CAMERA_ID, position)
 	
 	local chunkPosition = vmath.vector3(
-		math.floor((position.x + world_generation_constants.BLOCK_SIZE / 2) / world_generation_constants.BLOCK_SIZE / world_generation_constants.CHUNK_SIZE),
-		math.floor((position.y + world_generation_constants.BLOCK_SIZE / 2) / world_generation_constants.BLOCK_SIZE / world_generation_constants.CHUNK_SIZE),
+		math.floor((position.x + world_constants.BLOCK_SIZE / 2) / world_constants.BLOCK_SIZE / world_constants.CHUNK_SIZE),
+		math.floor((position.y + world_constants.BLOCK_SIZE / 2) / world_constants.BLOCK_SIZE / world_constants.CHUNK_SIZE),
 		0)
 	return chunkPosition
 end
