29bded776cfd197320b2eb36b55d02a40cf55cc8
JamesOsborne <james.osborne112@gmail.com>
2019-03-16 18:30:54 -0400

    Move directories around and add new brick blocks

---
+++ main/pickup/wood_planks/wood_planks_pickup_factory.go
@@ -0,0 +1,18 @@
+embedded_components {
+  id: "wood_planks_pickup_factory"
+  type: "factory"
+  data: "prototype: \"/main/pickup/wood_planks/wood_planks_pickup.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
