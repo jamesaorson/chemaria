d0c9213f5932a2d2d037c0906e8e2bc1629f1488
James Osborne <james.osborne112@gmail.com>
2019-03-23 01:31:50 -0400

    Remove login from deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,6 @@ chmod +x butler
 # just a sanity check run (and also helpful in case you're sharing CI logs)
 ./butler -V
 
-butler login
-
 butler push build/x86_64-darwin/Chemaria_x86_64-darwin.zip dangerfloof/chemaria:osx
 butler push build/x86_64-linux/Chemaria_x86_64-linux.zip dangerfloof/chemaria:linux
 butler push build/x86_64-win32/Chemaria_x86_64-win32.zip dangerfloof/chemaria:windows
