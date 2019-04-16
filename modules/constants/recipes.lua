cfb28f4cfc0dd98bd9f7657197b16106fa443585
JamesOsborne <james.osborne112@gmail.com>
2019-04-16 09:33:22 -0400

    Add affinity files and stone_pickaxe

--- modules/constants/recipes.lua
+++ modules/constants/recipes.lua
@@ -6,8 +6,14 @@ M.WOOD_PICKAXE = {
 	itemId = item_constants.WOOD_PICKAXE.id,
 	components = {}
 }
-M.WOOD_PICKAXE.components[item_constants.WOOD_PLANKS.id] = 2
-M.WOOD_PICKAXE.components[item_constants.STONE.id] = 3
+M.WOOD_PICKAXE.components[item_constants.WOOD_PLANKS.id] = 5
+
+M.STONE_PICKAXE = {
+	itemId = item_constants.STONE_PICKAXE.id,
+	components = {}
+}
+M.STONE_PICKAXE.components[item_constants.WOOD_PLANKS.id] = 3
+M.STONE_PICKAXE.components[item_constants.STONE.id] = 2
 
 M.WOOD_WALL = {
 	itemId = item_constants.WOOD_WALL.id,
