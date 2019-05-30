0d643ea9c2fbb56f3094b2c1f4f610a8f4537e3e
JamesOrson <james.osborne112@gmail.com>
2019-05-30 17:38:59 -0500

    Add load game menu. Reorganize project structure.

---
+++ main/menus/load_game_menu/load_game_menu.go
@@ -0,0 +1,15 @@
+components {
+  id: "load_game_menu"
+  component: "/main/menus/load_game_menu/load_game_menu.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
---
+++ main/player/ui/crafting_ui/crafting_ui.go
@@ -0,0 +1,15 @@
+components {
+  id: "crafting_ui"
+  component: "/main/player/ui/crafting_ui/crafting_ui.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
