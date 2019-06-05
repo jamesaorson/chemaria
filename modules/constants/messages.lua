f68c1bf202459f56d91475036159c935c84162fb
JamesOrson <james.osborne112@gmail.com>
2019-06-05 17:15:17 -0500

    Move all input and message hashes to a file and use the cached version

--- modules/constants/messages.lua
+++ modules/constants/messages.lua
@@ -1,9 +1,19 @@
 local M = {}
 
 M.CRAFT = hash("craft")
-
+M.CREATE_LOOSE_PICKUP = hash("create_loose_pickup")
+M.FURNACE = hash("furnace")
+M.MINE_BLOCK = hash("mine_block")
+M.PICKUP = hash("pickup")
+M.PROXY_LOADED = hash("proxy_loaded")
+M.SWITCH_PROXY = hash("switch_proxy")
+M.SWITCH_SELECTED_TOOLBAR_ITEM = hash("switch_selected_toolbar_item")
+M.SWITCH_SELECTED_TOOLBAR_ITEM = hash("switch_selected_toolbar_item")
+M.TRIGGER_RESPONSE = hash("trigger_response")
+M.UPDATE_CHUNK_RENDER = hash("update_chunk_render")
 M.UPDATE_INVENTORY = hash("update_inventory")
 M.UPDATE_RECIPE_LIST = hash("update_recipe_list")
 M.UPDATE_TOOLBAR = hash("update_toolbar")
+M.UPDATE_TOOLBAR_ITEM = hash("update_toolbar_item")
 
 return M
