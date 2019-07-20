459dd3a63a982acc485ba44a8c89d618a5a0b7f9
JamesOrson <james.osborne112@gmail.com>
2019-07-19 23:54:24 -0700

    Add pause ui and new toolbar/inventory files

---
+++ main/player/ui/pause_ui/pause_ui.go
@@ -0,0 +1,15 @@
+components {
+  id: "crafting_ui"
+  component: "/main/player/ui/pause_ui/pause_ui.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
